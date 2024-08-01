65cdebfa3835be7279efcc1c881f68ef74fc1ac1
import-sisi <zahra.ghanbari210@gmail.com>
2024-08-01 18:56:07 +0330

    add persistData

--- react-todolist/src/App.jsx
+++ react-todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
@@ -7,13 +7,19 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState('');
 
+  function persistData(newList){
+    localStorage.setItem('todos', JSON.stringify({ todos: newList}))
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList)
     setTodos(newTodoList);
   }
 
   function handleDeleteTodo(index) {
     const newTodoList = todos.filter((todo, todoIndex) => todoIndex !== index);
+    persistData(newTodoList)
     setTodos(newTodoList);
   }
 
@@ -22,6 +28,21 @@ function App() {
     setTodoValue(valueToBeEdited)
     handleDeleteTodo(index)
   }
+
+  useEffect(() => {
+    if (! localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem("todos")
+    if (! localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
 
   return (
     <>
